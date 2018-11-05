cbd3b9626fa5efd8a5bd82d70af3e50b4704934a
Adam <adam.brown@digital.justice.gov.uk>
2018-11-05 16:14:24 +0000

    Fix timezone php error

--- web/app/themes/imbmembers/templates/entry-meta.php
+++ web/app/themes/imbmembers/templates/entry-meta.php
@@ -3,7 +3,9 @@
 use Roots\Sage\Utils;
 
 $datetime = new DateTime(get_post_time('r', true));
-$datetime->setTimezone(new DateTimeZone(get_option('timezone_string')));
+
+//$datetime->setTimezone(new DateTimeZone(get_option('timezone_string')));
+
 $human_date = Utils\human_date($datetime);
 $attr_datetime = $datetime->format('c');
 $attr_title = $datetime->format(get_option('date_format')) . ' at ' . $datetime->format(get_option('time_format'));
