5d2316972acc28faf914a4a3b8a5a05380d0ffd2
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-11-13 11:30:12 +0000

    readding all the changes in to the new theme folder

--- web/app/themes/hmcts-km/functions.php
+++ web/app/themes/hmcts-km/functions.php
@@ -13,7 +13,6 @@ $sage_includes = [
   'lib/utils.php',                 // Utility functions
   'lib/Nav/Walkers/ButtonNavWalker.php', // Button Nav Walker class
   'lib/Nav/Walkers/TreeNavWalker.php', // Tree Nav Walker class
-  'lib/Users/UserRoles.php',       // MOJ User Roles
   'lib/init.php',                  // Initial theme setup and constants
   'lib/wrapper.php',               // Theme wrapper class
   'lib/conditional-tag-check.php', // ConditionalTagCheck class
@@ -27,6 +26,7 @@ $sage_includes = [
   'lib/taxonomies.php',            // Configure post taxonomies
   'lib/login.php',                 // Changes to login functionality
   'lib/change-password.php',       // Frontend 'change password' functionality
+  'lib/metabox.php'                // Extra Metabox
 ];
 
 foreach ($sage_includes as $file) {
