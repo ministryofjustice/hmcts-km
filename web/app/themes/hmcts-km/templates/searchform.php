f2f39d016546ac6e99cf357003e9a798f2d1c633
Adam <adam.brown@digital.justice.gov.uk>
2018-11-14 18:11:46 +0000

    Add category tags and search text
    Added category tags to the article excerpt and changed the search box
    suggested text.

--- web/app/themes/hmcts-km/templates/searchform.php
+++ web/app/themes/hmcts-km/templates/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" action="<?= esc_url(home_url('/')); ?>">
   <label class="sr-only"><?php _e('Search for:', 'sage'); ?></label>
   <div class="input-group">
-    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search pages...', 'sage'); ?>" required>
+    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search articles...', 'sage'); ?>" required>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'sage'); ?></button>
     </span>
