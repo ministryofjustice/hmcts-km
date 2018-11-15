d9888ff13a278c5384ec4873054539108fbbea13
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-11-15 15:34:32 +0000

    new permissions setting for author and editor

---
+++ web/app/themes/hmcts-km/lib/users/author.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Author User Role
+ *
+ * Remove capabilities from Author.
+ *
+ * Call the function when your plugin/theme is activated.
+ */
+
+function author_set_capabilities() {
+	// Get the role object.
+	$author = get_role( 'author' );
+	// A list of capabilities to remove from editors.
+	$caps = array(
+		'publish_posts',
+		'publish_pages',
+	);
+
+	foreach ( $caps as $cap ) {
+		// Remove the capability.
+		$author->remove_cap( $cap );
+	}
+}
+add_action( 'init', 'author_set_capabilities' );
