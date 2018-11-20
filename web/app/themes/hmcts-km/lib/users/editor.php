ab292236bc34b9f198c03e143c5a3e53ddc86539
Adam <adam.brown@digital.justice.gov.uk>
2018-11-20 10:32:07 +0000

    Add appearance menu for Editor role
    We want editors to be able to edit the two menus (lefthand page menu and
    quicklink top of knowledge article menu). This allows for that custom
    setting.

--- web/app/themes/hmcts-km/lib/users/editor.php
+++ web/app/themes/hmcts-km/lib/users/editor.php
@@ -35,3 +35,23 @@ function editor_set_capabilities() {
 	}
 }
 add_action( 'init', 'editor_set_capabilities' );
+
+// Allow editors to only see appearance menu
+$role_object = get_role( 'editor' );
+$role_object->add_cap( 'edit_theme_options' );
+
+function hide_menu() {
+
+    // Hide theme selection page
+    remove_submenu_page( 'themes.php', 'themes.php' );
+
+    // Hide widgets page
+    remove_submenu_page( 'themes.php', 'widgets.php' );
+
+    // Hide customize page
+    global $submenu;
+    unset($submenu['themes.php'][6]);
+
+}
+
+add_action('admin_head', 'hide_menu');
