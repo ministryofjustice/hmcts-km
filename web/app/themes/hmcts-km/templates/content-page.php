e848860a269c7c69610770fe3300b80b070fb2c5
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-11-23 10:55:16 +0000

    added the two text editors to pages

--- web/app/themes/hmcts-km/templates/content-page.php
+++ web/app/themes/hmcts-km/templates/content-page.php
@@ -1,3 +1,15 @@
+<?php
+  global $post;
+  $public_value = get_post_meta( $post->ID, '_public_editor', false );
+  $welsh_value = get_post_meta( $post->ID, '_welsh_editor', false );
+  if (!empty( $public_value ) || !empty( $welsh_value )){
+    $public_text = $public_value[0];
+    $welsh_text = $welsh_value[0];
+  }else{
+    $public_text = '';
+    $welsh_text = '';
+  }
+?>
 <article <?php post_class('entry'); ?>>
     <div class="entry-wrapper">
         <header>
@@ -5,8 +17,32 @@
             <?php get_template_part('templates/page-meta'); ?>
         </header>
         <div class="entry-content">
-            <?php the_content(); ?>
+          <?php
+            if(!empty(get_the_content())){
+              echo '<div class="app-contact-panel private">';
+              the_content();
+              echo '</div>';
+            }
+          ?>
         </div>
+        <div class="entry-content">
+    			<?php
+    			if(!empty($public_text)){
+    				echo '<div class="app-contact-panel public">';
+    					echo '<h1>Advice for your customer</h1>';
+    					echo '<p>' . $public_text . '</p>';
+
+    				if(!empty($welsh_text)){
+    					echo '<a data-toggle="collapse" data-target="#welsh" class="btn-hidden_panel" >View this in Welsh</a>';
+    					echo '<div id="welsh" class="collapse app-contact-panel welsh">';
+    						echo $welsh_text ;
+    					echo '</div>';
+    				}
+
+    				echo '</div>';
+    			}
+    			?>
+    		</div>
         <footer>
             <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
         </footer>
