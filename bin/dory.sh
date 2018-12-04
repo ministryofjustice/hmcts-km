1176c15b39b7213734f93b8cc85d2f65969db371
Adam <adam.brown@digital.justice.gov.uk>
2018-12-04 15:11:32 +0000

    Remove invalid charactor

--- bin/dory.sh
+++ bin/dory.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo mkdir -m 777 -p /private/etc/resolver/
-sudo touch -m 777 -p /private/etc/resolver/hmctskm.docker
+sudo touch -m 777 /private/etc/resolver/hmctskm.docker
 
 echo sh -c "# added by dory" >> /private/etc/resolver/hmctskm.docker
 echo sh -c "nameserver 127.0.0.1" >> /private/etc/resolver/hmctskm.docker
