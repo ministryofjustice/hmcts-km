fc7b6290c031fe2c3d2ed5a97007eb1bed9513bc
Adam <adam.brown@digital.justice.gov.uk>
2018-12-04 12:37:15 +0000

    Force mkdir

--- bin/dory.sh
+++ bin/dory.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo mkdir -p /home/private/etc/resolver/
-sudo touch /home/private/etc/resolver/hmctskm.docker
+sudo mkdir -m 777 -p /private/etc/resolver/
+sudo touch -m 777 -p /private/etc/resolver/hmctskm.docker
 
-echo sh -c "# added by dory" >> /home/private/etc/resolver/hmctskm.docker
-echo sh -c "nameserver 127.0.0.1" >> /home/private/etc/resolver/hmctskm.docker
-echo sh -c "port 53" >> /home/private/etc/resolver/hmctskm.docker
+echo sh -c "# added by dory" >> /private/etc/resolver/hmctskm.docker
+echo sh -c "nameserver 127.0.0.1" >> /private/etc/resolver/hmctskm.docker
+echo sh -c "port 53" >> /private/etc/resolver/hmctskm.docker
