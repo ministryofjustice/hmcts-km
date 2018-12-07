31d877b4a57e2230451328b7c5fdda81bed26539
Adam <adam.brown@digital.justice.gov.uk>
2018-12-07 14:12:18 +0000

    Add category search checkbox

--- web/app/themes/hmcts-km/templates/searchform.php
+++ web/app/themes/hmcts-km/templates/searchform.php
@@ -2,6 +2,10 @@
   <label class="sr-only"><?php _e('Search for:', 'sage'); ?></label>
   <div class="input-group">
     <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search articles...', 'sage'); ?>" required>
+    <div>
+      <input type="checkbox" id="category" name="category" value="cat">
+      <label for="category">Search by category</label>
+    </div>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'sage'); ?></button>
     </span>
