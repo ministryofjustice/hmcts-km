cfe876a2b1d65b6ac7ec6fffadf37ed878bd72c2
Adam <adam.brown@digital.justice.gov.uk>
2019-01-18 17:10:53 +0000

    Apply CSS design to cat search filter
    New search filter has been created but needs CSS styling appled. These
    changes present a new search filter layout with search and filter added.

--- web/app/themes/hmcts-km/lib/utils.php
+++ web/app/themes/hmcts-km/lib/utils.php
@@ -7,69 +7,64 @@ use DateTime;
 /**
  * Tell WordPress to use searchform.php from the templates/ directory
  */
-function get_search_form()
-{
-    $form = '';
-    locate_template('/templates/searchform.php', true, false);
-    return $form;
+function get_search_form() {
+	$form = '';
+	locate_template( '/templates/searchform.php', true, false );
+	return $form;
 }
-add_filter('get_search_form', __NAMESPACE__ . '\\get_search_form');
+add_filter( 'get_search_form', __NAMESPACE__ . '\\get_search_form' );
 
 /**
  * Make a URL relative
  */
-function root_relative_url($input)
-{
-    preg_match('|https?://([^/]+)(/.*)|i', $input, $matches);
-    if (!isset($matches[1]) || !isset($matches[2])) {
-        return $input;
-    } elseif (($matches[1] === $_SERVER['SERVER_NAME']) || $matches[1] === $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']) {
-        return wp_make_link_relative($input);
-    } else {
-        return $input;
-    }
+function root_relative_url( $input ) {
+	preg_match( '|https?://([^/]+)(/.*)|i', $input, $matches );
+	if ( ! isset( $matches[1] ) || ! isset( $matches[2] ) ) {
+		return $input;
+	} elseif ( ( $matches[1] === $_SERVER['SERVER_NAME'] ) || $matches[1] === $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] ) {
+		return wp_make_link_relative( $input );
+	} else {
+		return $input;
+	}
 }
 
 /**
  * Compare URL against relative URL
  */
-function url_compare($url, $rel)
-{
-    $url = trailingslashit($url);
-    $rel = trailingslashit($rel);
-    if ((strcasecmp($url, $rel) === 0) || root_relative_url($url) == $rel) {
-        return true;
-    } else {
-        return false;
-    }
+function url_compare( $url, $rel ) {
+	$url = trailingslashit( $url );
+	$rel = trailingslashit( $rel );
+	if ( ( strcasecmp( $url, $rel ) === 0 ) || root_relative_url( $url ) == $rel ) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /**
  * Check if element is empty
  */
-function is_element_empty($element)
-{
-    $element = trim($element);
-    return !empty($element);
+function is_element_empty( $element ) {
+	$element = trim( $element );
+	return ! empty( $element );
 }
 
 /**
- * Human-friendly date string
+ * Human-friendly date string:
  * "about 2 hours ago"
  * "on 15/02/2015"
  *
  * @param DateTime $date
  * @return string
  */
-function human_date($date)
-{
-    $friendly_cutoff = new DateTime('-1 month');
+function human_date( $date ) {
+	$friendly_cutoff = new DateTime( '-1 month' );
 
-    if ($date < $friendly_cutoff) {
-        return $date->format(get_option('date_format'));
-    } else {
-        return human_time_diff($date->getTimestamp()) . ' ago';
-    }
+	if ( $date < $friendly_cutoff ) {
+		return $date->format( get_option( 'date_format' ) );
+	} else {
+		return human_time_diff( $date->getTimestamp() ) . ' ago';
+	}
 }
 
 /**
@@ -77,7 +72,6 @@ function human_date($date)
  *
  * @return boolean
  */
-function is_wp_cli()
-{
-    return defined('WP_CLI') && WP_CLI;
+function is_wp_cli() {
+	return defined( 'WP_CLI' ) && WP_CLI;
 }
--- web/app/themes/hmcts-km/templates/searchform.php
+++ web/app/themes/hmcts-km/templates/searchform.php
@@ -1,14 +1,19 @@
 <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 
   <label class="sr-only"><?php _e( 'Search for:', 'sage' ); ?></label>
-  <div class="input-group">
-	<input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e( 'Search articles...', 'sage' ); ?>" required>
-	<div class="input-group">
+
+  <div class="input-group mb-3">
+
+	<input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e( 'Search by keyword', 'sage' ); ?>">
+
+  </div>
+
+	<div class="input-group-append">
 		<?php
 
 		wp_dropdown_categories(
 			[
-				'show_option_all' => 'All posts',
+				'show_option_all' => 'Pages and articles',
 				'orderby'         => 'name',
 				'echo'            => 1,
 				'selected'        => $cat,
@@ -21,9 +26,11 @@
 		);
 
 		?>
+	Or search by selecting a specific category.
 	</div>
-	<span class="input-group-btn">
-	  <button type="submit" class="search-submit btn btn-default"><?php _e( 'Search', 'sage' ); ?></button>
-	</span>
+
+  <div class="input-group-prepend">
+	<button type="submit" class="search-submit btn btn-default"><?php _e( 'Search', 'sage' ); ?></button>
   </div>
+
 </form>
