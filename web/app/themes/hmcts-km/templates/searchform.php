48db233fd4602970afcc17368d46d8a4a6156a9f
Adam <adam.brown@digital.justice.gov.uk>
2019-01-18 15:33:30 +0000

    Add category filter to search
    A filter has been added that allows users to filter search results based
    on category, not just by keyword.

--- web/app/themes/hmcts-km/templates/searchform.php
+++ web/app/themes/hmcts-km/templates/searchform.php
@@ -1,13 +1,29 @@
-<form role="search" method="get" action="<?= esc_url(home_url('/')); ?>">
-  <label class="sr-only"><?php _e('Search for:', 'sage'); ?></label>
+<form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+
+  <label class="sr-only"><?php _e( 'Search for:', 'sage' ); ?></label>
   <div class="input-group">
-    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search articles...', 'sage'); ?>" required>
-    <div>
-      <input type="checkbox" id="category" name="category" value="cat">
-      <label for="category">Search by category</label>
-    </div>
-    <span class="input-group-btn">
-      <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'sage'); ?></button>
-    </span>
+	<input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e( 'Search articles...', 'sage' ); ?>" required>
+	<div class="input-group">
+		<?php
+
+		wp_dropdown_categories(
+			[
+				'show_option_all' => 'All posts',
+				'orderby'         => 'name',
+				'echo'            => 1,
+				'selected'        => $cat,
+				'hierarchical'    => true,
+				'query_var'       => true,
+				'class'           => 'dropdown-menu',
+				'id'              => 'custom-cat-drop',
+				'value_field'     => 'term_id',
+			]
+		);
+
+		?>
+	</div>
+	<span class="input-group-btn">
+	  <button type="submit" class="search-submit btn btn-default"><?php _e( 'Search', 'sage' ); ?></button>
+	</span>
   </div>
 </form>
