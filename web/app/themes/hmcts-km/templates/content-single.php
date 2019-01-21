5891475145ae9badb305dddb20ee15a29c1c47d3
Adam <adam.brown@digital.justice.gov.uk>
2019-01-21 13:16:29 +0000

    Text change, advice to guidance on page & post
    HMCTS requested a text change on pages and post to read Guidance for
    your customers, rather then advice for them.

--- web/app/themes/hmcts-km/templates/content-single.php
+++ web/app/themes/hmcts-km/templates/content-single.php
@@ -1,14 +1,14 @@
 <?php
 	global $post;
 	$public_value = get_post_meta( $post->ID, '_public_editor', false );
-	$welsh_value = get_post_meta( $post->ID, '_welsh_editor', false );
-	if (!empty( $public_value ) || !empty( $welsh_value )){
-		$public_text = $public_value[0];
-		$welsh_text = $welsh_value[0];
-	}else{
-		$public_text = '';
-		$welsh_text = '';
-	}
+	$welsh_value  = get_post_meta( $post->ID, '_welsh_editor', false );
+if ( ! empty( $public_value ) || ! empty( $welsh_value ) ) {
+	$public_text = $public_value[0];
+	$welsh_text  = $welsh_value[0];
+} else {
+	$public_text = '';
+	$welsh_text  = '';
+}
 ?>
 <article <?php post_class( 'entry' ); ?>>
 	<div class="entry-wrapper">
@@ -18,33 +18,33 @@
 		</header>
 		<div class="entry-content">
 			<?php
-				if(!empty(get_the_content())){
-					echo '<div class="app-contact-panel private">';
-					the_content();
-					echo '</div>';
-				}
+			if ( ! empty( get_the_content() ) ) {
+				echo '<div class="app-contact-panel private">';
+				the_content();
+				echo '</div>';
+			}
 			?>
 		</div>
 		<div class="entry-content">
 			<?php
-			if(!empty($public_text)){
+			if ( ! empty( $public_text ) ) {
 				echo '<div class="app-contact-panel public">';
-					echo '<h1>Advice for your customer</h1>';
+					echo '<h1>Guidance for your customers</h1>';
 					echo '<p>' . $public_text . '</p>';
 
-				if(!empty($welsh_text)){
+				if ( ! empty( $welsh_text ) ) {
 					echo '<a data-toggle="collapse" data-target="#welsh" class="btn-hidden_panel" >View this in Welsh</a>';
 					echo '<div id="welsh" class="collapse app-contact-panel welsh">';
-						echo $welsh_text ;
+						echo $welsh_text;
 					echo '</div>';
 				}
 
 				echo '</div>';
 			}
-?>
+			?>
 <br />
 <hr>
-Article tagged as: <?php the_category(', '); ?>
+Article tagged as: <?php the_category( ', ' ); ?>
 		</div>
 
 		<footer>
@@ -55,7 +55,7 @@ Article tagged as: <?php the_category(', '); ?>
 					'after'  => '</p></nav>',
 				]
 			);
-?>
+			?>
 		</footer>
 	</div>
 </article>
