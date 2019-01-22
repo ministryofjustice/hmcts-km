5841ce58716091ff0e1a1929b0a2f2f9d2b91bc2
Adam <adam.brown@digital.justice.gov.uk>
2019-01-22 11:59:33 +0000

    Fix layout issue when logged in

--- web/app/themes/hmcts-km/templates/sidebar.php
+++ web/app/themes/hmcts-km/templates/sidebar.php
@@ -24,7 +24,5 @@ $user = wp_get_current_user();
 </nav>
 <hr class="hidden-md hidden-lg hidden-sm">
 <div class="widget widget_logout hidden-md hidden-lg hidden-sm">
-    <p class="text-center"><?= esc_html("{$user->user_firstname} {$user->user_lastname}") ?></p>
-    <a href="<?= get_the_permalink($changePassword) ?>" class="btn btn-block btn-link"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span> <?= get_the_title($changePassword) ?></a>
     <a href="<?= wp_logout_url() ?>" class="btn btn-block btn-link"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout</a>
 </div>
